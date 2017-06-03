823d05dd3d7ff5654a0bc2b2ae8097cd2b78d482
Andrey Radionov <andyomsk@gmail.com>
2017-06-04 02:13:23 +0300

    Initial

---
+++ src/main/java/com/radionov/jrbot/dto/RequestMessageDTO.java
@@ -0,0 +1,75 @@
+package com.radionov.jrbot.dto;
+
+import com.radionov.jrbot.model.Conversation;
+import com.radionov.jrbot.model.User;
+
+import java.util.Date;
+
+/**
+ * @author Andrey Radionov
+ */
+public class RequestMessageDTO {
+    private String id;
+    private String text;
+    private String type;
+    private Date timestamp;
+    private Conversation conversation;
+    private User from;
+    private User recipient;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
+    }
+
+    public User getFrom() {
+        return from;
+    }
+
+    public void setFrom(User from) {
+        this.from = from;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
+}
