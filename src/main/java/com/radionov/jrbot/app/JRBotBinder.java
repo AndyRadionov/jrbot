419f56bc0cf4fdd6e0955686061a978ed395d4cb
Andrey Radionov <andyomsk@gmail.com>
2017-06-05 14:14:05 +0300

    joke

--- src/main/java/com/radionov/jrbot/app/JRBotBinder.java
+++ src/main/java/com/radionov/jrbot/app/JRBotBinder.java
@@ -18,10 +18,11 @@ import javax.ws.rs.client.ClientBuilder;
 public class JRBotBinder extends AbstractBinder {
     @Override
     protected void configure() {
-        bind(MessageServiceImpl.class).to(MessageService.class).in(Singleton.class);
         bindFactory(JerseyClientFactory.class).to(Client.class).in(Singleton.class);
         bind(JokeMessageProcessorImpl.class).named("jokeMessageProcessor")
-                .to(MessageProcessor.class).in(Singleton.class);
+                .to(MessageProcessor.class).in(Singleton.class);bind(MessageServiceImpl.class).to(MessageService.class).in(Singleton.class);
+
+
     }
 
     private static class JerseyClientFactory implements Factory<Client> {
